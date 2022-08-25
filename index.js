600dffd78a1898e6359c96d2ac61415bc5c95579
lidya <lidya. abraham@student.moringaschool.com>
2022-08-25 13:07:25 +0300

    commpleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob';
+console.log(customerName);
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+var bestCustomer;
+
+function setBestCustomer() { 
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = 'toy';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'adix';
+    return leastFavoriteCustomer;
+}
